4a5e770af615d4e2de5ace4bacb4b1469d7c2fbc
beauwilliams <beau.williams@icloud.com>
2022-07-13 12:39:53 +1000

    add: save audits to audit dir

--- tasks/audit.ts
+++ tasks/audit.ts
@@ -1,20 +1,21 @@
-
-import { spawnSync } from 'child_process';
-import { writeFileSync } from 'fs';
+import { spawnSync } from "child_process";
+import { mkdirSync, existsSync, writeFileSync } from "fs";
 import { Signer } from "@ethersproject/abstract-signer";
 import { task } from "hardhat/config";
 //Static security analysis via Slither
-task(
-  "audit",
-  "Runs an internal audit using Slither"
-).setAction(async () => {
+task("audit", "Runs an internal audit using Slither").setAction(async () => {
   const child = spawnSync(
-    "slither", [
-    "--filter-paths", "node_modules",
-    "--exclude", "naming-convention",
-    ".", "--checklist"
-  ], { encoding: 'utf-8' });
+    "slither",
+    ["--filter-paths", "node_modules", "--exclude", "naming-convention", ".", "--checklist"],
+    { encoding: "utf-8" },
+  );
 
   console.log(child.stderr);
-  writeFileSync("audit.md", child.stdout);
+  var dir = "./audit";
+
+  //NOTE: make audit/ dir if not exist already
+  if (!existsSync(dir)) {
+    mkdirSync(dir);
+  }
+  writeFileSync("audit/audit.md", child.stdout);
 });
