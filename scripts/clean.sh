772cb96485c391484840e89e4ed86c087c7f0e19
beauwilliams <beau.williams@icloud.com>
2022-09-05 11:20:23 +1000

    feat: add script to clean the template when starting a new project

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Cleaning up..."
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
+    No ) exit;;
+  esac
+done
