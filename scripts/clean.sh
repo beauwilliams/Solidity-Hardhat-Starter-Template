4b9539d51a29c0acabafd3e73ff37d810ff13e56
beauwilliams <beau.williams@icloud.com>
2022-09-27 19:31:52 +1000

    fix: clean script now removes everything

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,7 +3,7 @@
 echo "Cleaning up..."
 select yn in "Yes" "No"; do
   case $yn in
-    Yes ) rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy && rm -rf ./scripts/upgrade && rm -rf ./scripts/verify && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
+    Yes ) rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy.ts && rm -rf ./scripts/upgrade.ts && rm -rf ./scripts/verify.ts && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
     No ) exit;;
   esac
 done
