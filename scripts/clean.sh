b34aee304af158aca4dc92a29ffd03bc795c3712
Beau <beau.williams@icloud.com>
2022-09-27 19:33:48 +1000

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,7 +3,7 @@
 echo "Cleaning up..."
 select yn in "Yes" "No"; do
   case $yn in
-    Yes ) rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy.ts && rm -rf ./scripts/upgrade.ts && rm -rf ./scripts/verify.ts && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
+    Yes ) rm -rf ./.git && rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy.ts && rm -rf ./scripts/upgrade.ts && rm -rf ./scripts/verify.ts && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
     No ) exit;;
   esac
 done
