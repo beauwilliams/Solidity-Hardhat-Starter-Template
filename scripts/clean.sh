3f1f64dd56f56f7884d8e63668b30a62ddde19a1
Beau <beau.williams@icloud.com>
2022-09-30 13:57:33 +1000

    fix: break from clean case, so we can continue to install package case

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,7 +4,7 @@ echo "Cleaning up..."
 select yn in "Yes" "No"; do
   case $yn in
     Yes ) rm -rf ./.git && rm -rf ./artifacts/* && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy.ts && rm -rf ./scripts/upgrade.ts && rm -rf ./scripts/verify.ts && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
-    No ) exit;;
+    No ) break;;
   esac
 done
 
